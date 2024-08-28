522bb270f7f67a913489e42bd51cb3eb382ff07c
Larry <Larryh2612>
2024-08-28 19:18:58 -0400

    Version 1.1

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,24 @@
+describe('calculateMonthlyPayment should return correct figures', function(){
+  it('should calculate the monthly rate correctly', function () {
+    expect(calculateMonthlyPayment({amount: 300000, years: 25, rate: 7.99})).toBe('2313.46');
+  });
+  
+  
+  it("should return a result with 2 decimal places", function() {
+    expect(calculateMonthlyPayment({amount: 300000, years: 25, rate: 7.99})).toBe('2313.46');
+  });
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
+
 });
 
 
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
 
+
+
+// describe('updateMonthly should display the correct amount', function(){
+//     let val = document.getElementById('monthly-payment').innerText;
+//   it('should display the correct amount on the UI', function(){
+//     expect(val).toBe('2313.46');
+//   })
+// });
 /// etc
